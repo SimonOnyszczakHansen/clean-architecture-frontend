43ed8b1611c28312a4d8d4c9f04acc19451124d4
SimonOnyszczakHansen <107280264+SimonOnyszczakHansen@users.noreply.github.com>
2026-09-04 11:20:48 +0200

    added semicolon

--- src/app/infrastructure/config/auth.guard.ts
+++ src/app/infrastructure/config/auth.guard.ts
@@ -3,7 +3,7 @@ import { CanActivateFn, Router } from "@angular/router";
 import { AuthStatusPort } from "../../domain/ports/auth-status.port";
 
 export const authGuard: CanActivateFn = async () => {
-	const authStatus = inject(AuthStatusPort)
+	const authStatus = inject(AuthStatusPort);
 	const router = inject(Router);
 	if (await authStatus.isAuthenticated()) {
 		return true;
